a610ebe263d431690c12d83e59ff1be4479a81db
Mateus Schiavo Rosseto <161091856+MateusSRosseto@users.noreply.github.com>
2025-10-06 20:49:01 -0300

    Update streamlit_app.py
    Putting the API call inside the for fruit_chosen for loop.

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,8 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         ingredients_string += fruit_chosen + ' '
 
     #st.write(ingredients_string)
@@ -49,6 +51,3 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
